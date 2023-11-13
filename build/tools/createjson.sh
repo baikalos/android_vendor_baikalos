8912747c1ccf9c34e98015768f87d7f65a50c4ef
Sergei Vasilev <svasiliev22@yandex.ru>
2023-11-13 06:55:05 +0300

    Fix sorceforge path
    Change-Id: I453ec72fc741d88fa54adf6c65a8760d2ff30c0f

--- build/tools/createjson.sh
+++ build/tools/createjson.sh
@@ -97,7 +97,7 @@ if [ -f $existingOTAjson ]; then
 			"oem": "'$oem'",
 			"device": "'$device'",
 			"filename": "'$filename'",
-			"download": "https://sourceforge.net/projects/baikalos/files/'$1'/'$v_max'.x/'$3'/download",
+			"download": "https://sourceforge.net/projects/baikalos/files/'$1'/'$3'/download",
 			"timestamp": '$timestamp',
 			"md5": "'$md5'",
 			"sha256": "'$sha256'",
