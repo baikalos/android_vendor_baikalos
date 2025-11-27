efa1a93ba59119bc3664d7d058dce16ab1a56550
Sergei Vasilev <svasiliev22@yandex.ru>
2025-11-27 20:05:35 +0300

    Baikalify 1
    Change-Id: I14783481e33e3ec5f625b00bfc49db2d1796f84c

--- config/version.mk
+++ config/version.mk
@@ -1,17 +1,19 @@
 PRODUCT_VERSION_MAJOR = 16
-PRODUCT_VERSION_MINOR = 0
+PRODUCT_VERSION_MINOR = 2
 
 # Increase CR Version with each major release.
-CR_VERSION := 12.11
+CR_VERSION := 16.2
+BAIKAL_VERSION := 16.2
 
 # Internal version
-LINEAGE_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(CR_VERSION)
+#LINEAGE_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(CR_VERSION)
+LINEAGE_VERSION := BaikalOS-$(PRODUCT_VERSION_MAJOR)-$(LINEAGE_BUILD)-DEV-$(shell date +%Y%m%d)
 
 # Display version
-LINEAGE_DISPLAY_VERSION := v$(CR_VERSION)-$(shell date +%Y%m%d)
+LINEAGE_DISPLAY_VERSION := v$(BAIKAL_VERSION)-$(shell date +%Y%m%d)
 
 # LineageOS version properties
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.crdroid.build.version=$(CR_VERSION) \
-    ro.crdroid.display.version=$(LINEAGE_DISPLAY_VERSION) \
-    ro.crdroid.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.baikalos.build.version=$(BAIKAL_VERSION) \
+    ro.baikalos.display.version=$(LINEAGE_DISPLAY_VERSION) \
+    ro.baikalos.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)
