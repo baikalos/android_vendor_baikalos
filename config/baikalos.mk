f5d075a57aad1382eee5c86241f421d5e518b3d4
Sergei Vasilev <svasiliev22@yandex.ru>
2025-11-27 20:05:35 +0300

    Baikalify 1
    Change-Id: I14783481e33e3ec5f625b00bfc49db2d1796f84c

--- config/baikalos.mk
+++ config/baikalos.mk
@@ -29,7 +29,7 @@ endif
 
 # Cloned app exemption
 PRODUCT_COPY_FILES += \
-    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-baikal-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-baikal-product.xml
 
 # ColumbusService
 ifneq ($(TARGET_SUPPORTS_QUICK_TAP),false)
