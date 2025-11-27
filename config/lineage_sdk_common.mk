3704d67e7214b107d8387b3f9c62d7f2f6357992
Sergei Vasilev <svasiliev22@yandex.ru>
2025-11-27 20:05:35 +0300

    Baikalify 1
    Change-Id: I14783481e33e3ec5f625b00bfc49db2d1796f84c

--- config/lineage_sdk_common.mk
+++ config/lineage_sdk_common.mk
@@ -1,12 +1,12 @@
 # Permissions for lineage sdk services
 PRODUCT_COPY_FILES += \
-    vendor/lineage/config/permissions/org.lineageos.globalactions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.globalactions.xml \
-    vendor/lineage/config/permissions/org.lineageos.hardware.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.hardware.xml \
-    vendor/lineage/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml \
-    vendor/lineage/config/permissions/org.lineageos.livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.livedisplay.xml \
-    vendor/lineage/config/permissions/org.lineageos.profiles.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.profiles.xml \
-    vendor/lineage/config/permissions/org.lineageos.settings.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.settings.xml \
-    vendor/lineage/config/permissions/org.lineageos.trust.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.trust.xml
+    vendor/lineage/config/permissions/org.baikalos.globalactions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.baikalos.globalactions.xml \
+    vendor/lineage/config/permissions/org.baikalos.hardware.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.baikalos.hardware.xml \
+    vendor/lineage/config/permissions/org.baikalos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.baikalos.health.xml \
+    vendor/lineage/config/permissions/org.baikalos.livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.baikalos.livedisplay.xml \
+    vendor/lineage/config/permissions/org.baikalos.profiles.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.baikalos.profiles.xml \
+    vendor/lineage/config/permissions/org.baikalos.settings.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.baikalos.settings.xml \
+    vendor/lineage/config/permissions/org.baikalos.trust.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.baikalos.trust.xml
 
 # Lineage Platform Library
 PRODUCT_PACKAGES += \
@@ -19,4 +19,8 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/framework/oat/%/org.lineageos.platform.odex \
     system/framework/oat/%/org.lineageos.platform.vdex \
     system/framework/org.lineageos.platform-res.apk \
-    system/framework/org.lineageos.platform.jar
+    system/framework/org.lineageos.platform.jar \
+    system/framework/oat/%/org.mineageos.platform.odex \
+    system/framework/oat/%/org.mineageos.platform.vdex \
+    system/framework/org.mineageos.platform-res.apk \
+    system/framework/org.mineageos.platform.jar
